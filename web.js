75058a478b2f695247bbf61efe1209e7a4b38bbf
k_vin92@yahoo.com <ubuntu@ip-172-31-30-71.us-west-2.compute.internal>
2013-07-22 13:12:29 +0000

    change web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+var file = fs.readFileSync("index.html");
+var filetext = file.toString();
+
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(filetext);
 });
 
 var port = process.env.PORT || 5000;
